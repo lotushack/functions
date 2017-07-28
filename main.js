b178434f7a0ac32ec692b1bdebd276dc49379c70
Alonya Lafayette <alonyalafayette1@gmail.com>
2017-07-28 02:37:10 -0400

    fixed minor issues

--- main.js
+++ main.js
@@ -5,22 +5,23 @@
 function max(num1, num2){
   if (num1 > num2) {
     return num1;
-  }else if { (num2 > num1)
+  } else if (num2 > num1){
     return num2;
+  }
 }
 
-max(10,23)
+console.log(max(10,23));
 
 // 2.
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
 function maxOfThree(num1, num2, num3){
-  if (num1 > num2 & num3){
+  if (num1 > num2 && num1 > num3){
     return num1;
-  } else if { (num2 > num1 & num3)
+  } else if  (num2 > num1 && num2 > num3){
     return num2;
-  }else if { (num3 > num1 & num2)
+  }else if  (num3 > num1 && num3 > num2){
     return num3;
   }
 }
@@ -31,41 +32,45 @@ console.log(maxOfThree(3, 5, 13));
 // Then, write and example of using the function.
 
 function isVowel(char){
-  if (char  === a && e && i && o && u){
+  if (char  === "a" || char === "e" ||char === "i" ||char === "o" ||char === "u"){
     return "true"
-  } else { "false"
+  } else {
+    return "false"
+
   }
 
 }
-isVowel(Alonya)
+
+console.log(isVowel("o"));
 
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
-function sum(x + y){
+function sum(x, y){
   return x + y
 }
-sum(3,4)
+
+console.log(sum(3,4));
 
 
 
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // Then, write and example of using the function.
-function average(x, y, z)/2 {
-  return (x + y + z)/2
+function average(x, y, z) {
+  return (x + y + z)/3
 }
 
-average(10, 20, 30)
+console.log(average(10, 20, 30));
 
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // Then, write and example of using the function.
-function getLength("Alonya"){
-  return getLength.length
+function getLength(x){
+  return x.length
 }
-getLength(Alonya)
+console.log(getLength("Alonya"));
 
 // 7.
 // Write a function called `greaterThan` that takes two parameters
@@ -74,12 +79,12 @@ getLength(Alonya)
 // Then, write and example of using the function.
 function greaterThan(x,y){
   if (x < y) {
-    return "true"{
-    }  else {
+    return "true"
+  }  else if (x > y) {
     return "false"
     }
 }
-greaterThan(12, 40)
+console.log(greaterThan(12, 40));
 
 
 // 8.
@@ -89,11 +94,11 @@ greaterThan(12, 40)
 // is the parameter that was passed in.
 // Then, write and example of using the function.
 function greet (Name){
-  return "Hello " + Name;
-  console.log("Hello, " + Name "!");
-}
+  return "Hello " + Name + "!";
 
-greet(Alonya)
+}
+console.log(greet("Alonya"));
+
 
 
 // 9.
@@ -105,7 +110,6 @@ greet(Alonya)
 // sentence: "quick brown fox jumps over the fence"
 // Then, write and example of using the function.
 function madlib (w, x, y, z){
-  return (baby, milk, night, sleep);
-  console.log("The " + w + " cried for" + x + " all" + y + " unitil she went to " + z + ".");
+  return ("The " + w + " cried for " + x + " all " + y + " until she went to " + z + ".");
 }
-madlib("The " + w + " cried for" + x + " all" + y + " unitil she went to " + z + ".")
+console.log(madlib("baby", "milk", "night", "sleep"))
